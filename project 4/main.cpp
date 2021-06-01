43002b7f151cb3b05af48ceed9e22bfff0a2d39f
Arian <arinovano@gmail.coom>
2021-06-01 14:56:56 -0700

    Initial Commit

---
+++ project 4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project 4
+//
+//  Created by Arian Jafari mimandi on 6/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
